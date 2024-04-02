84b94f30650147895c864ba1e561df3a494cbb20
Rahul Kashyap <30754662+itisrmk@users.noreply.github.com>
2024-04-02 14:13:58 -0700

    rest

--- FlaskApplication/app.py
+++ FlaskApplication/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 import datetime
+import os
 app = Flask(__name__)
 app.debug = True
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/stocks'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -32,7 +40,26 @@ def portfolio():
 
 @app.route("/tracsaction")
 def tracsaction():
-    return render_template('tracsaction.html')
+    stock = Stock.query.all()
+    return render_template('tracsaction.html', stock=stock)
+
+
+
+#Models 
+class Stock(db.Model):
+    __tablename__ = 'stock'
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.String(10))
+    price = db.Column(db.Float)
+    date = db.Column(db.DateTime, default=func.now())
+
+    def __init__(self, ticker, price):
+        self.ticker = ticker
+        self.price = price
+    
+    def __repr__(self):
+        return f'{self.ticker} at {self.price}'
+
 
 if(__name__ == "__main__"): 
-    app.run(debug=True) 
+    app.run(debug=True)
